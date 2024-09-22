1c08e282c8493e1fe00861e5c89c9e0397c859cb
Mohammad Afiq <46773936+afiqzx@users.noreply.github.com>
2024-09-22 12:18:40 +0800

    LED flip flop

--- src/main.c
+++ src/main.c
@@ -11,20 +11,23 @@ void do_noop(long count) {
 
 int main(void) {
     volatile register unsigned long *pin0 = (void *)(P1CR_BASE);
-    volatile register unsigned long *pin0d = (pin0 + 0x02);
 
-    *pin0d |= (1<<12);
+    // Data direction register is 16 bit shifted
+    *pin0 |= (1<<(11 + 16));
+    *pin0 |= (1<<(12 + 16));
     
     int lighted = 0;
     while(1) {
-        do_noop(9999);
+        do_noop(999999);
 
         if (lighted == 0) {
-            *pin0 |= (1<<12);
+            *pin0 |= (1<<11);
+            *pin0 &= ~(1<<12);
             lighted = 1;
         }
         else {
-            *pin0 &= ~(1<<12);
+            *pin0 &= ~(1<<11);
+            *pin0 |= (1<<12);
             lighted = 0;
         }
     }
